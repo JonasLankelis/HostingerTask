bff2f9dc792028e7875c09f58f72c0cd272795cb
Jonas Lankelis <jonslankelis@gmail.com>
2018-11-11 04:13:47 +0200

    fixed env keys, page is ready to go

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Categories list</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div style="margin-top: 20px; margin-bottom: 20px;">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
