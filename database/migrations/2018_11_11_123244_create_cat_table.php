bdf968f0e638002128432400659d4a3b76a5287d
Jonas Lankelis <jonslankelis@gmail.com>
2018-11-11 14:56:55 +0200

    fixed database config, created category table

--- database/migrations/2018_11_11_123244_create_cat_table.php
+++ database/migrations/2018_11_11_123244_create_cat_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCatTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('cat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cat_name', 80);
+            $table->string('parent_id')->references('id')
+                    ->on('cat');
         });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('cat');
     }
 }
